ba81adb75b55315e4817c4839b064e97ba2b78b6
davinhster <vincentnishikino@gmail.com>
2019-11-15 15:22:33 -0800

    Revert "Merge branch 'master' of https://github.com/ryan1tran/swap-class"
    This reverts commit 0da8ba882b96ae875636d49b104dbeddabf97bb8, reversing
    changes made to bb8f8c12a87a271ad22b4fd4f479bd2f6c5a842c.

--- node-starter-files/server.js
+++ node-starter-files/server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'SJSU Class Scheduler',
+    title: 'SJSU Swap Class',
     courses: courses.classes,
   });
 });
